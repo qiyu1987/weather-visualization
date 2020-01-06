6e78f773a6f906d834500836c5cccbd2711afbd3
qiyu1987 <qiyu1987@gmail.com>
2020-01-06 15:38:34 +0100

    refactory style outside

--- src/visualization/main.js
+++ src/visualization/main.js
@@ -60,6 +60,14 @@ class Visualization extends SizedComponent {
 			// change the height
 			const yMax = this.getY(d.max_temp)
 			const yMin = this.getY(d.min_temp)
+			// color bar based on average temporature
+			const avg_temp = (d.max_temp + d.min_temp) / 2
+			// assuming range from (-5, 30), we can later changed to base on data
+			const avg_red = 255 * ((avg_temp + 5) / 35)
+			const avg_blue = 255 - avg_red
+			const colorStyle = {
+				fill: `rgb(${avg_red},50,${avg_blue})`
+			}
 			bars.push(
 				<rect
 					x={x}
@@ -67,10 +75,7 @@ class Visualization extends SizedComponent {
 					width={w}
 					height={yMin - yMax}
 					key={d.date}
-					style={{
-						fill: `rgb(${((d.max_temp + 10) / 50) * 255},
-                            0,${255 - ((d.max_temp + 10) / 50) * 255})`
-					}}
+					style={colorStyle}
 				/>
 			)
 			x += barWidth
